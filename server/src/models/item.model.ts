9bd355c73e3c95572a887e329c9279174027c194
Rai5559 <icexbee1@gmail.com>
2023-07-04 14:37:11 -0600

    asfdkhjg

--- server/src/models/item.model.ts
+++ server/src/models/item.model.ts
@@ -1,6 +1,6 @@
 import { Exclude, Expose } from "class-transformer";
 import { DataTypes } from "sequelize";
-import { AllowNull, CreatedAt, Model, UpdatedAt } from "sequelize-typescript";
+import { AllowNull, CreatedAt, Default, Model, UpdatedAt } from "sequelize-typescript";
 import { AutoIncrement, Column, PrimaryKey, Table } from "sequelize-typescript";
 
 // NOTE: sequelize-typescript complains if column decorator is not at bottom of decorator stack
@@ -34,4 +34,131 @@ class Item extends Model<Item> {
   updatedOn: Date;
 }
 
-export default Item;
+@Table
+class User extends  Model<User>{
+  @Expose()
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataTypes.INTEGER)
+  id: number;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  name: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  email: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  password: string;
+
+  @Expose()
+  @Default(true)
+  @Column
+  isActive: boolean;
+
+  @CreatedAt
+  creationDate: Date;
+
+  @UpdatedAt
+  updatedOn: Date;
+
+
+}
+
+@Table
+class Wallet extends Model<Wallet>{
+  @Expose()
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataTypes.INTEGER)
+  id: number;
+
+  
+  @Expose()
+  @AllowNull(false)
+  @Column
+  currency: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  balance: number;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  userId: number;
+
+  @CreatedAt
+  creationDate: Date;
+
+  @UpdatedAt
+  updatedOn: Date;
+
+}
+
+@Table
+class Movement extends Model<Movement>{
+
+  @Expose()
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataTypes.INTEGER)
+  id: number;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  sender: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  receiver: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  amount: number;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  currency: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  type: string;
+
+  @Expose()
+  @AllowNull(false)
+  @Column
+  walletId: number;
+
+  @CreatedAt
+  creationDate: Date;
+
+  @UpdatedAt
+  updatedOn: Date;
+
+}
+
+
+User.hasOne(Wallet, {foreignKey: 'userId'});
+Wallet.belongsTo(User, {foreignKey: 'userId'});
+Wallet.hasMany(Movement, {foreignKey: 'walletId'});
+Movement.belongsTo(Wallet, {foreignKey: 'walletId'});
+
+
+export { Item, User, Wallet };
+
+
+
+
