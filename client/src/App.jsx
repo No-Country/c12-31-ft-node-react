2b038db9c29fc6c21887f64bf8c88c12dd21d7c4
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-07-04 14:00:01 -0300

    HU08-REGISTRO-MODIFICADO

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Registro from "./components/Registro"
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-    </div>
+    <>
+    <Registro/>
+    </>
   )
 }
 
