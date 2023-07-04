47a7965371626079dc4e568913b1292b995361e2
Exequiel <exequielcostabel@gmail.com>
2023-07-04 15:58:02 -0300

    HU08LoginPage

--- client/src/components/LoginScreen/LoginScreen.jsx
+++ client/src/components/LoginScreen/LoginScreen.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const LoginScreen = () => {
+
+      //STATES
+      const [email, setEmail] = useState("");
+      const [contraseña, setContraseña] = useState("");
+      const [pais, setPais] = useState("")
+      const [error, setError] = useState(false)
+
+      const [mensaje, setMensaje] = useState("")
+  
+    
+    const handleSubmit = ( e ) => {
+        e.preventDefault();
+
+         //VALIDACION
+         if(email === "" || contraseña === "" ||  pais === ""  ){
+            setError(true)
+            setMensaje("Los campos deben estar completos ")
+            return
+        }else{
+            setError(false)
+            console.log(email, contraseña, pais)
+        }
+    }
+
   return (
     <div className='p-10 mt-10 flex-col bg-zinc-50'>
         <h1 className='text-xl font-semibold mb-3'>Iniciar sesión</h1>
         <form action="" className='bg-zinc-300 rounded-3xl p-7 f'>
 
-        <div className="flex-col flex">
-            <label htmlFor="pais" className="text-lg">Pais</label>
+        {error && <p className="text-red-500 text-center text-lg">{mensaje}</p>}
+
+        <div className="flex-col flex py-1">
+            <p className="font-semibold">Pais</p>
             <select 
-            className="h-10 p-2 rounded-full bg-zinc-200"
+                className="h-10 p-2 rounded-full bg-zinc-200"
+                value={pais}
+                onChange={(e) => setPais(e.target.value)}
             >
                 <option value="">--Selecciona--</option>
                 <option value="argentina">Argentina</option>
@@ -19,24 +47,42 @@ export const LoginScreen = () => {
         </div>
 
             <div className='py-1'>
-                <p className='mb-1 font-semibold'>Ingresa tu nombre de usuario o email</p>
-                <input type="text" className='bg-zinc-200 rounded-full px-2 w-full h-10'/>
+                <p className='mb-1 font-semibold'>Ingresa tu email</p>
+                <input 
+                    type="email" 
+                    value={email} 
+                    className='bg-zinc-200 rounded-full px-2 w-full h-10'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
 
             <div className='py-1'>
                 <p className='mb-1 font-semibold'>Contraseña</p>
-                <input type="password" placeholder='******' className='bg-zinc-200 rounded-full px-2 w-full h-10'/>
+                <input 
+                    type="password" 
+                    placeholder='******' 
+                    className='bg-zinc-200 rounded-full px-2 w-full h-10'
+                    value={contraseña}
+                    onChange={(e) => setContraseña(e.target.value)}
+                />
             </div>
         </form>
 
         <div className='flex justify-center mt-5'>
-            <button className='bg-zinc-300 px-6 py-2 rounded-full font-semibold'>
+            <button 
+            onClick={handleSubmit}
+            className='bg-zinc-300 px-6 py-2 rounded-full font-semibold'
+            >
                 Iniciar Sesión
             </button>
         </div>
 
         <div className='flex justify-center mt-10'>
-            <button className='bg-zinc-300 w-full rounded-lg py-2 font-semibold'>Iniciar sesiòn con Google</button>
+            <button 
+            className='bg-zinc-300 w-full rounded-lg py-2 font-semibold' 
+            >
+                Iniciar sesiòn con Google
+            </button>
         </div>
 
 
@@ -46,8 +92,6 @@ export const LoginScreen = () => {
 
         <div className='flex justify-center mt-10 font-semibold'>
             <small>Por consultas o ayudas escribenos en ...</small><hr />
-            
-            
         </div>
     </div>
   )
