1f597884ddd82e7323161d8d57b9f79d0a45a450
Rai5559 <icexbee1@gmail.com>
2023-07-05 14:37:14 -0600

    fixed:walletrepository/YRH

--- server/src/services/wallet.service.ts
+++ server/src/services/wallet.service.ts
@@ -4,18 +4,18 @@ import Wallet from "src/models/wallet.model";
 import { HttpError } from "src/types/http-error.type";
 
 export class WalletService {
-  private static readonly userRepository = sequelize.getRepository(Wallet);
+  private static readonly walletRepository = sequelize.getRepository(Wallet);
 
   public static async create(
     userId: number,
     currency: string
   ): Promise<Wallet> {
-    const wallet = await this.userRepository.create({ userId, currency });
+    const wallet = await this.walletRepository.create({ userId, currency });
     return wallet;
   }
 
   public static async findByUserId(userId: number): Promise<Wallet[]> {
-    const wallet = await this.userRepository.findAll({
+    const wallet = await this.walletRepository.findAll({
       where: { userId: userId },
     });
     if (!wallet) {
@@ -25,7 +25,7 @@ export class WalletService {
   }
 
   public static async findOne(walletId: number): Promise<Wallet> {
-    const wallet = await this.userRepository.findByPk(walletId);
+    const wallet = await this.walletRepository.findByPk(walletId);
     if (!wallet) {
       throw new HttpError(404, "Wallet not found");
     }
@@ -38,7 +38,7 @@ export class WalletService {
   ): Promise<Wallet> {
     const wallet = await this.findOne(walletId);
     wallet.balance_pesos = wallet.balance_pesos + updateBalance;
-    await this.userRepository.update(wallet, {
+    await this.walletRepository.update(wallet, {
       where: { id: walletId },
     });
     return wallet;
@@ -50,7 +50,7 @@ export class WalletService {
   ): Promise<Wallet> {
     const wallet = await this.findOne(walletId);
     wallet.balance_dollars = wallet.balance_dollars + updateBalance;
-    await this.userRepository.update(wallet, {
+    await this.walletRepository.update(wallet, {
       where: { id: walletId },
     });
     return wallet;
@@ -61,7 +61,7 @@ export class WalletService {
     updateWalletDto: UpdateWalletDto
   ): Promise<Wallet> {
     const wallet = await this.findOne(walletId);
-    await this.userRepository.update(updateWalletDto, {
+    await this.walletRepository.update(updateWalletDto, {
       where: { id: walletId },
     });
     return wallet;
