4736c7df1311f0e1b7597314dc8ffbd9566d7be3
Araldi Garcia <96481537+Aizendaisho@users.noreply.github.com>
2023-07-14 08:51:16 -0400

    Revert "Revert "Hu22 conectar el front con el back""

--- client/src/components/Registro/Registro.jsx
+++ client/src/components/Registro/Registro.jsx
@@ -1,56 +1,84 @@
-import { useState } from "react"
-import { Link } from "react-router-dom";
-import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai"
+
+import { useState, useContext } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { register } from "../../services/api";
+import { UserContext } from "../../context/useUserContext";
+import { useNavigate } from "react-router-dom";
+import { setTokenInCookies } from "../../utils/cookiesFn";
 
 
 function Registro() {
+  const { login } = useContext(UserContext);
 
   //STATES
   const [email, setEmail] = useState("");
-  const [contraseña, setContraseña] = useState("");
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [password, setPassword] = useState("");
   const [repetirContraseña, setRepetirContraseña] = useState("");
-  const [pais, setPais] = useState("")
-  const [error, setError] = useState(false)
-  const [exito, setExito] = useState(false)
-  const [mensaje, setMensaje] = useState("")
-  const [contraseñaError, setContraseñaError] = useState(false)
+  const [country, setCountry] = useState("");
+  const [error, setError] = useState(false);
+  const [exito, setExito] = useState(false);
+  const [mensaje, setMensaje] = useState("");
+  const [contraseñaError, setContraseñaError] = useState(false);
+  const navegate = useNavigate();
 
   //MOSTRAR CONTRASEÑA
-  const [mostrarContraseña1, setMostrarContraseña1] = useState(false)
-  const [mostrarContraseña2, setMostrarContraseña2] = useState(false)
+  const [mostrarContraseña1, setMostrarContraseña1] = useState(false);
+  const [mostrarContraseña2, setMostrarContraseña2] = useState(false);
 
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/;
+  
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     //VALIDACION
-    if (email === "" || contraseña === "" || repetirContraseña === "" || pais === "" || contraseña !== repetirContraseña) {
+    if (
+      email === "" ||
+      password === "" ||
+      name === "" ||
+      lastname === "" ||
+      repetirContraseña === "" ||
+      country === "" ||
+      password !== repetirContraseña
+    ) {
+      setError(true);
+      setExito(false);
+      setMensaje(
+        "Los campos deben de estan completos y las contraseñas deben ser iguales"
+      );
+      return;
+    } else {
+      if (!passwordRegex.test(password)) {
+        setContraseñaError(true);
 
-      setError(true)
-      setExito(false)
-      setMensaje("Los campos deben de estan completos y las contraseñas deben ser iguales")
-      return
+        setExito(false);
+        return;
+      } else {
+        setExito(true);
+        setError(false);
+        setMensaje("Se registro correctamente");
+        setContraseñaError(false);
+        const userForm = {
+          name,
+          lastname,
+          email,
+          country,
+          password,
+        };
 
-    }
-    else {
-      if (!passwordRegex.test(contraseña)) {
+        const respuesta = await register(userForm);
+        if (respuesta.statusCode === 201) {
+          login(respuesta.data);
+          setTokenInCookies(respuesta.data.access_token);
+          navegate("/dashboard");
+        }
 
-        setContraseñaError(true)
-
-        setExito(false)
-        return
+        //ENVIO DE DATOS
       }
-      else {
-        setExito(true)
-        setError(false)
-        setMensaje("Se registro correctamente")
-        setContraseñaError(false)
-      }
     }
-
-
-  }
+  };
 
   const handleToggleContraseña1 = (event) => {
     event.preventDefault();
@@ -64,18 +92,46 @@ function Registro() {
 
   return (
     <div className="bg-white">
-      <h2 className="text-center text-black font-bol m-12 text-xl">Estas por registrar un nuevo usuario en tu billetera virtual</h2>
-
-
+      <h2 className="text-center text-black font-bol m-12 text-xl">
+        Estas por registrar un nuevo usuario en tu billetera virtual
+      </h2>
 
       <div className="flex justify-center items-center">
         <form className="flex flex-col gap-4 p-10 m-5 w-96 bg-[#D9D9D9] rounded-2xl h-full ">
           <div className="h-4 m-2">
-            {error && <p className="text-red-500 text-center text-sm">{mensaje}</p>}
-            {exito && <p className="text-green-500 text-center text-lg">{mensaje}</p>}
+            {error && (
+              <p className="text-red-500 text-center text-sm">{mensaje}</p>
+            )}
+            {exito && (
+              <p className="text-green-500 text-center text-lg">{mensaje}</p>
+            )}
           </div>
           <div className="flex-col flex">
-            <label htmlFor="mail" className="text-lg ">Ingresa un email</label>
+            <label htmlFor="nombre" className="text-lg ">
+              Ingresa tu Nombre
+            </label>
+            <input
+              type="text"
+              className="rounded-2xl h-10 p-2"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex-col flex">
+            <label htmlFor="apellido" className="text-lg ">
+              Ingresa tu apellido
+            </label>
+            <input
+              type="text"
+              className="rounded-2xl h-10 p-2"
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+          <div className="flex-col flex">
+            <label htmlFor="mail" className="text-lg ">
+              Ingresa un email
+            </label>
             <input
               type="email"
               className="rounded-2xl h-10 p-2"
@@ -84,30 +140,44 @@ function Registro() {
             />
           </div>
 
-
           <div>
-            <label htmlFor="contraseña text-xl">Contraseña</label>
+            <label htmlFor="password text-xl">Contraseña</label>
             <div className="relative">
               <input
                 type={mostrarContraseña1 ? "text" : "password"}
-                id="contraseña"
+                id="password"
                 className="rounded-2xl h-10 p-2 pr-28"
-                onChange={(e) => setContraseña(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <button
                 className="absolute right-2 top-2 bg-transparent border-none"
                 onClick={handleToggleContraseña1}
               >
-                {mostrarContraseña1 ? <AiFillEye className="text-gray-500"/> : <AiFillEyeInvisible  className="text-gray-500"/>}
+                {mostrarContraseña1 ? (
+                  <AiFillEye className="text-gray-500" />
+                ) : (
+                  <AiFillEyeInvisible className="text-gray-500" />
+                )}
               </button>
             </div>
           </div>
 
-
-          {contraseñaError ? <p className="text-sm text-center m-2 text-red-600">Debe tener minimo 6 caracteres, una mayuscula, una minuscula, un numero.</p> : <p className="text-sm text-center m-2">Debe tener minimo 6 caracteres, una mayuscula, una minuscula, un numero.</p>}
+          {contraseñaError ? (
+            <p className="text-sm text-center m-2 text-red-600">
+              Debe tener minimo 6 caracteres, una mayuscula, una minuscula, un
+              numero.
+            </p>
+          ) : (
+            <p className="text-sm text-center m-2">
+              Debe tener minimo 6 caracteres, una mayuscula, una minuscula, un
+              numero.
+            </p>
+          )}
 
           <div>
-            <label htmlFor="repetirContraseña text-xl">Repetir Contraseña</label>
+            <label htmlFor="repetirContraseña text-xl">
+              Repetir Contraseña
+            </label>
             <div className="relative">
               <input
                 type={mostrarContraseña2 ? "text" : "password"}
@@ -119,18 +189,23 @@ function Registro() {
                 className="absolute right-2 top-2 bg-transparent border-none"
                 onClick={handleToggleContraseña2}
               >
-                {mostrarContraseña2 ? <AiFillEye className="text-gray-500"/> : <AiFillEyeInvisible  className="text-gray-500"/>}
-
+                {mostrarContraseña2 ? (
+                  <AiFillEye className="text-gray-500" />
+                ) : (
+                  <AiFillEyeInvisible className="text-gray-500" />
+                )}
               </button>
             </div>
           </div>
 
           <div className="flex-col flex">
-            <label htmlFor="pais" className="text-lg">Pais</label>
+            <label htmlFor="country" className="text-lg">
+              Pais
+            </label>
             <select
               className="rounded-2xl h-10 p-2"
-              value={pais}
-              onChange={(e) => setPais(e.target.value)}
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
             >
               <option value="">--Selecciona--</option>
               <option value="argentina">Argentina</option>
@@ -138,27 +213,27 @@ function Registro() {
               <option value="brasil">Brasil</option>
             </select>
           </div>
-
-
         </form>
       </div>
 
       <div className="flex justify-center items-center">
-        <Link to={"/pagWelcome"}>
+        
         <button
           className="text-lg bg-[#D9D9D9] p-4 rounded-2xl mt-10"
           onClick={handleSubmit}
-        >Registrarse</button>
-        </Link>
+
+        >
+          Registrarse
+        </button>
+
       </div>
 
-
       <p className="text-center m-8 text-lg">
-        Por consultas o ayuda escribenos en tubilleteravirtual@nombrebilletera.com
+        Por consultas o ayuda escribenos en
+        tubilleteravirtual@nombrebilletera.com
       </p>
-
     </div>
-  )
+  );
 }
 
-export default Registro
+export default Registro;
---
+++ client/src/config/env.config.js
@@ -0,0 +1,3 @@
+export const baseURL = "http://localhost:3000/api"
+
+// import.meta.env.VITE_BASE_URL_API || 
