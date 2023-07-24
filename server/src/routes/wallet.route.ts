d3e1cde40b6db92da3312f94951311cd2289bf5e
CarlosFernandoMoreno <carlosfernandomoreno@hotmail.com>
2023-07-24 17:40:07 -0300

    littles changes

--- server/src/routes/wallet.route.ts
+++ server/src/routes/wallet.route.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { WalletController } from "controllers/wallet.controller";
-import { TransactionController } from "controllers/transaction.controller";
 import { VerifyToken } from "middleware/verifyToken.middleware";
 const router = Router();
 
 router.post("/transaction", VerifyToken, WalletController.depositPesos);
-router.get("/transaction", VerifyToken, TransactionController.findAll);
-router.get("/transaction/:id", WalletController.findOne);
+router.get("/transaction/:id", VerifyToken, WalletController.findOne);
 export { router as walletRouter };
