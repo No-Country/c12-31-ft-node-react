84e2a77ecf809466adb9870bf85d38905beb1959
Exequiel <exequielcostabel@gmail.com>
2023-07-24 22:09:40 -0300

    AHORA SI

--- server/src/routes/wallet.route.ts
+++ server/src/routes/wallet.route.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { WalletController } from "controllers/wallet.controller";
 import { TransactionController } from "controllers/transaction.controller";
+import {
+  depositPesosValidator,
+  getWalletByIdValidator,
+} from "validators/wallet.validator";
+
 const router = Router();
 
-router.post("/transaction", WalletController.depositPesos);
+router.post(
+  "/transaction",
+  depositPesosValidator,
+  WalletController.depositPesos
+);
 router.get("/transaction", TransactionController.findAll);
-router.get("/transaction/:id", WalletController.findOne);
+router.get(
+  "/transaction/:id",
+  getWalletByIdValidator,
+  WalletController.findOne
+);
 export { router as walletRouter };
