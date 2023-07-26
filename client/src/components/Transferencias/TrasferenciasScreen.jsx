edfc208636608110790d12398d8352361562e2a9
Exequiel <exequielcostabel@gmail.com>
2023-07-26 11:35:13 -0300

    arreglo tarjeta y transferencias

--- client/src/components/Transferencias/TrasferenciasScreen.jsx
+++ client/src/components/Transferencias/TrasferenciasScreen.jsx
@@ -29,7 +29,7 @@ export const TrasferenciasScreen = () => {
 
     const getTransactions = async() => {
       const data = await allTransation();
-      console.log(data)
+      
     }
 
     useEffect(() => {
